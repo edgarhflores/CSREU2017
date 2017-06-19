759c91d0e26a54eb543b06e4e04153644007e2ff
Edgar <eflores@samford.edu>
2017-06-19 22:59:01 +0100

    Finished LBP Alg, need to work on outputting image

--- LoopyBeliefProp/LoopyBelief.cpp
+++ LoopyBeliefProp/LoopyBelief.cpp
@@ -15,47 +15,50 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <limits>
 #include "node.h"
+//constants
 const int WINDOW_SIZE = 5;
 const int MAX_OFF_SET = 15;
 const int LAMDA = 20;
 const int TRUNCATE = 2;
+//
 int numOfFiles = 1;
-int imageWidthLeft;
-int imageWidthRight;
-int leftSize;
-int rightSize;
+//widths
+int leftImageWidth;
+int rigthImageWidth;
+//heights
+int leftImageHeight;
+//int rightImageHeight;
+//sizes
+int leftSize; //does this need to be a global?
+int rightSize; //does this need to be a global?
+//image arrays
 int* leftImageArray;
 int* rightImageArray;
-node* leftImageNodes;
+//node arrays
+node* tempData;
+node* prevData;
+node* currData;
+
 using namespace std;
 
-void updateMessage (int i){
-    
-}
 
-void updateBelief(int i){
-
-}
-
-void loopyBP(){
-    for (int i = 0; i < leftSize; i++) {
-        updateMessage(i);
-        updateBelief(i);
-    }
-
-}
 //********************************************************************
 //Method:	smoothnessCost
 //Description:	calculates smoothnessCost
-//Parameters:  	
-//Returns:     	nothing
+//Parameters:  	k - the current node
+//              kPrime - the neighbor node
+//Returns:     	smoothnessCost - the penalization of adjacent labels that are 
+//                               different
 //Calls:       	nothing
-//Globals:	none
-int smoothnessCost(int k, int kPrime){
-    int n = abs (k - kPrime);
+//Globals:	LAMDA
+//              TRUNCATE
+
+int smoothnessCost(int k, int kPrime) {
+    int n = abs(k - kPrime);
     int smoothnessCost;
-    smoothnessCost = LAMDA * min(n,TRUNCATE);
+    smoothnessCost = LAMDA * min(n, TRUNCATE);
     return smoothnessCost;
 }
 
@@ -65,6 +68,7 @@ int smoothnessCost(int k, int kPrime){
 //              coordinate 
 //Parameters:  	x - the x coordinate
 //              y - the y coordinate
+//     imageWidth -  the width of an image
 //Returns:     	index - the coordinates of single dimensional index
 //Calls:       	nothing
 //Globals:	none
@@ -86,14 +90,16 @@ int getIndex(int x, int y, int imageWidth) {
 //Returns:     	sumOfAbsDiff - the sum of absolute difference for every pixel in 
 //              one window
 //Calls:       	nothing
-//Globals:	none
+//Globals:	WINDOW_SIZE
+//              imageWidthLeft
+//              imageWidthRight
 
-int dataCost(int x, int y, int label, int leftImage[], int rightImage[]) {
+int dataCost(int x, int y, int kPrime) {
     int sumOfAbsDiff;
     int border = WINDOW_SIZE / 2;
     for (int i = x - border; i <= x + border; i++) {
         for (int j = y - border; j < y + border; j++) {
-            sumOfAbsDiff = abs(leftImage[getIndex(i, j, imageWidthLeft)] - rightImage[getIndex(i - label, j, imageWidthRight)]);
+            sumOfAbsDiff += abs(leftImageArray[getIndex(i, j, leftImageWidth)] - rightImageArray[getIndex(i - kPrime, j, rigthImageWidth)]);
         }//inner for loop
     }//outer for loop
     return sumOfAbsDiff;
@@ -103,7 +109,7 @@ int dataCost(int x, int y, int label, int leftImage[], int rightImage[]) {
 //Method:	printArray
 //Description:	print array
 //Parameters:  	array[] - the array being printed
-//              size    - the width * height of an image in a PGM file
+//                 size - the width * height of an image in a PGM file
 //Returns:     	nothing
 //Calls:       	nothing
 //Globals:	none
@@ -114,21 +120,24 @@ void printArray(int array[], int size) {
     }
 }
 
-void printNodeArray(node array[], int size) {
-    for (int index = 0; index < size; index++) {
-        node newNode;
-        newNode = node (array[index]);
-        cout << newNode.getGrayScaleValue() << endl;
-    }
-}
+//void printNodeArray(node array[], int size) {
+//    for (int index = 0; index < size; index++) {
+//        node currentNode;
+//        currentNode = node(array[index]);
+//        cout << currentNode.getGrayScaleValue() << endl;
+//    }
+//}
 
 //********************************************************************
 //Method:	getSize
-//Description:	this method calculates the width and height of an image 
+//Description:	this method calculates the width and height of an image and sets
+//              the imageWidthLeft and imageWidthRight
 //Parameters:  	ifstream& fileObj - this is the current PGM file
 //Returns:     	size - the width * height of an image in a PGM file
 //Calls:       	nothing
-//Globals:	none
+//Globals:	imageWidthLeft
+//              imageWidthRight
+//              numOfFiles
 
 int getSize(ifstream& fileObj) {
     string input;
@@ -143,9 +152,10 @@ int getSize(ifstream& fileObj) {
     size = imageWidth * imageHeight;
     getline(fileObj, input); // this is the maximum gray scale value
     if (numOfFiles == 0) {
-        imageWidthLeft = imageWidth;
+        leftImageHeight = imageHeight;
+        leftImageWidth = imageWidth;
     } else {
-        imageWidthRight = imageWidth;
+        rigthImageWidth = imageWidth;
     }
     return size;
 }
@@ -169,14 +179,14 @@ void putValuesIntoArray(int *array, ifstream& fileObj, int size) {
     }// End of for loop
 }// End of putValuesIntoArray function
 
-void createLeftNodeArray(int *array, int size, node *nodeArray) {
-    int value;
-    for (int i = 0; i < size; i++) {
-        value =  array[i];
-        node newNode(value);
-        nodeArray[i] = newNode;
-    }// End of for loop
-}// End of createLeftNodeArray function
+//void createLeftNodeArray(int *array, int size, node *nodeArray) {
+//    int value;
+//    for (int i = 0; i < size; i++) {
+//        value = array[i];
+//        node newNode(value);
+//        nodeArray[i] = newNode;
+//    }// End of for loop
+//}// End of createLeftNodeArray function
 
 //********************************************************************
 //Method:	putValuesIntoArray
@@ -186,7 +196,14 @@ void createLeftNodeArray(int *array, int size, node *nodeArray) {
 //Returns:     	nothing 
 //Calls:       	getSize
 //              putValuesIntoArray
-//Globals:	none
+//Globals:	numOfFiles
+//              leftSize
+//              rightSize
+//              leftImageArray
+//              rightImageArray
+//              currentRoundData
+//              previousRoundData
+//              tempRoundData
 
 void setFiles() {
     char filename[50];
@@ -223,12 +240,14 @@ void setFiles() {
             }// end of switch       
         } else {
             cout << "Creating image array of PGM file...\n" << endl;
-            if (numOfFiles == 1) {              
+            if (numOfFiles == 1) {
                 leftSize = getSize(fileObj);
                 leftImageArray = new int[leftSize];
-                leftImageNodes = new node[leftSize];
+                currData = new node[leftSize];
+                prevData = new node[leftSize];
+                tempData = new node[leftSize];
                 putValuesIntoArray(leftImageArray, fileObj, leftSize);
-                createLeftNodeArray(leftImageArray, leftSize, leftImageNodes);
+                //createLeftNodeArray(leftImageArray, leftSize, currentRoundData);
                 cout << "The left image array was created\n" << endl;
             } else {
                 rightSize = getSize(fileObj);
@@ -241,7 +260,107 @@ void setFiles() {
         }// end of else
     }//end of while loop
 }
+void updateMessage(int x, int y) {
+    double minVal;
+    int top = getIndex(x, y - 1, leftImageWidth);
+    int bot = getIndex(x, y + 1, leftImageWidth);
+    int left = getIndex(x - 1, y, leftImageWidth);
+    int right = getIndex(x + 1, y, leftImageWidth);
+    //update top message
+    for (int k = 0; k < MAX_OFF_SET; k++) {
+        minVal = numeric_limits<int>::max();
+        for (int kPrime = 0; kPrime < 15; kPrime++) {
+            int tempMsg = dataCost(x, y, kPrime) + smoothnessCost(k, kPrime) +
+                    prevData[bot].getMsgTop(kPrime) +
+                    prevData[left].getMsgRight(kPrime) +
+                    prevData[right].getMsgLeft(kPrime);
+            if (tempMsg < minVal) {
+                minVal = tempMsg;
+            }//End of if statement     
+        }//End of inner loop
+        currData[getIndex(x, y, leftImageWidth)].setMsgTop(k, minVal);
+    }//End of outer loop
 
+    //update bottom message
+    for (int k = 0; k < MAX_OFF_SET; k++) {
+        minVal = numeric_limits<int>::max();
+        for (int kPrime = 0; kPrime < 15; kPrime++) {
+            int tempMsg = dataCost(x, y, kPrime) + smoothnessCost(k, kPrime) +
+                    prevData[top].getMsgBottom(kPrime) +
+                    prevData[left].getMsgRight(kPrime) +
+                    prevData[right].getMsgLeft(kPrime);
+            if (tempMsg < minVal) {
+                minVal = tempMsg;
+            }//End of if statement  
+
+        }//End of inner loop
+        currData[getIndex(x, y, leftImageWidth)].setMsgBottom(k, minVal);
+    }//End of outer loop
+
+    //update left message
+    for (int k = 0; k < MAX_OFF_SET; k++) {
+        minVal = numeric_limits<int>::max();
+        for (int kPrime = 0; kPrime < 15; kPrime++) {
+            int tempMsg = dataCost(x, y, kPrime) + smoothnessCost(k, kPrime) +
+                    prevData[top].getMsgBottom(kPrime) +
+                    prevData[bot].getMsgTop(kPrime) +
+                    prevData[right].getMsgLeft(kPrime);
+            if (tempMsg < minVal) {
+                minVal = tempMsg;
+            }//End of if statement     
+        }//End of inner loop
+        currData[getIndex(x, y, leftImageWidth)].setMsgLeft(k, minVal);
+    }//End of outer loop
+
+    //update right message
+    for (int k = 0; k < MAX_OFF_SET; k++) {
+        minVal = numeric_limits<int>::max();
+        for (int kPrime = 0; kPrime < 15; kPrime++) {
+            int tempMsg = dataCost(x, y, kPrime) + smoothnessCost(k, kPrime) +
+                    prevData[top].getMsgBottom(kPrime) +
+                    prevData[bot].getMsgTop(kPrime) +
+                    prevData[left].getMsgRight(kPrime);
+            if (tempMsg < minVal) {
+                minVal = tempMsg;
+            }//End of if statement     
+        }//End of inner loop
+        currData[getIndex(x, y, leftImageWidth)].setMsgRight(k, minVal);
+    }//End of outer loop
+    
+}// End of updateMessage
+
+void updateBelief(int x, int y) {
+    double belief;
+    int top = getIndex(x, y - 1, leftImageWidth);
+    int bot = getIndex(x, y + 1, leftImageWidth);
+    int left = getIndex(x - 1, y, leftImageWidth);
+    int right = getIndex(x + 1, y, leftImageWidth);
+    
+    for (int k = 0; k < MAX_OFF_SET ; k++) {
+        belief = dataCost(x,y,k) + prevData[top].getMsgBottom(k) +
+                    prevData[bot].getMsgTop(k) +
+                    prevData[left].getMsgRight(k) + prevData[right].getMsgLeft(k);
+        currData[getIndex(x, y, leftImageWidth)].setBelief(k, belief);
+    }
+}
+
+void loopyBP() {
+
+    for (int k = 0; k <= 15; k++) {
+        //need to loop through x and y values
+        for (int x = 0; x < leftImageWidth; x++) {
+            for (int y = 0; y < leftImageHeight; y++) {
+                //swap pointers
+                tempData = prevData;
+                prevData = currData;
+                currData = tempData;
+                //update data
+                updateMessage(x,y); // need to pass in x and y values
+                //updateBelief();
+            }// End of for loop for y
+        }// End of for loop for x
+    }//End of for loop for iterations
+}// End of loopyBP function
 //********************************************************************
 //Method:	main
 //Description:	This main method opens two files and stores the contents of both
@@ -257,39 +376,32 @@ int main() {
     string input;
     int userAns;
     bool loop = true;
-    int x;
-    int y;
+    int x = 3;
+    int y = 3;
+    node currentNode;
     cout << "Loopy Belief Propagation: Edgar Flores\n" << endl;
     setFiles();
-    while (loop) {
-        cout << "1 = Calculate DataCost (default)" << endl;
-        cout << "2 = End program" << endl;
-        getline(cin, input);
-        stringstream(input) >> userAns;
-        cout << endl;
-        switch (userAns) {
-            case 1:
+    cout << "setting pixel (3,3)'s (in currentRoundData) MsgBottom vector at position 2' value to 23... " << endl;
+    currData[getIndex(x, y, leftImageWidth)].setMsgBottom(2, 23);
+    int value;
+    value = int (currData[getIndex(x, y, leftImageWidth)].getMsgBottom(2));
 
-                break;
-            case 2:
-                exit(0);
-            default:
-                cout << "Input not valid. Please enter either 1 or 2.\n" << endl;
-        }// end of switch  
-    }//End of while loop 
+    cout << "The pixel (3,3)'s MsgBottom vector at position 2' value is: " << value << endl;
+
+    //    while (loop) {
+    //        cout << "1 = Calculate DataCost (default)" << endl;
+    //        cout << "2 = End program" << endl;
+    //        getline(cin, input);
+    //        stringstream(input) >> userAns;
+    //        cout << endl;
+    //        switch (userAns) {
+    //            case 1:
+    //                break;
+    //            case 2:
+    //                exit(0);
+    //            default:
+    //                cout << "Input not valid. Please enter either 1 or 2.\n" << endl;
+    //        }// end of switch  
+    //    }//End of while loop 
     return 0;
 }
-// Calculating the dataCost
-//                cout << "Enter value for x: " << endl;
-//                getline(cin, input);
-//                stringstream(input) >> x;
-//                cout << "Enter value for y: " << endl;
-//                getline(cin, input);
-//                stringstream(input) >> y;
-//                cout << "Calculating the dataCost...\n" << endl;
-//                int dataCostSum;
-//                for (int offset = 0; offset <= MAX_OFF_SET; offset++) {
-//                    dataCostSum += dataCost(x, y, offset, leftImageArray, rightImageArray);
-//                }
-//                cout << "Data cost for a window of 5x5: " << dataCostSum << endl;
-//                cout << endl;
